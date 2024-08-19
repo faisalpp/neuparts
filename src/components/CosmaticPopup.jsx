dba869eb543fb99934f6184d3260d4f7674aa694
Muhammad Fahad <mfd84739@gmail.com>
2024-08-19 23:54:18 +0500

    cosmetic popup added

--- src/components/CosmaticPopup.jsx
+++ src/components/CosmaticPopup.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { IoCloseOutline } from 'react-icons/io5';
-import Link from 'next/link';
-import Image from 'next/image';
 import { FaRegCircle } from 'react-icons/fa';
 import FourStar from './svgs/FourStar';
 
--- src/components/Modal/CustomModal.jsx
+++ src/components/Modal/CustomModal.jsx
@@ -1,7 +1,4 @@
 import React from 'react';
-import NeuShieldPopup from '../NeuShieldPopup';
-import FreeCurbsideReturn from '../FreeCurbsideReturn';
-import CompleteLaundary from '../CompleteLaundary';
 import CosmaticPopup from '../CosmaticPopup';
 
 const CustomModal = ({ openModal, closeModal, data }) => {
