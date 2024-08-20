0b08a8eb96bb7b6aa53b80b4ac307dc14a501b42
Muhammad Fahad <mfd84739@gmail.com>
2024-08-20 18:22:10 +0500

    changed mongodb

--- src/app/api/media/links/route.js
+++ src/app/api/media/links/route.js
@@ -1,40 +1,40 @@
-import Media from '@/models/media'
+import Media from '@/models/media';
 import { NextResponse } from 'next/server';
-import {connect} from '@/DB/index'
-import {GetYoutubeCode} from '@/utils/index'
+import connect from '@/lib/db';
+import { GetYoutubeCode } from '@/utils/index';
 
-export async function POST(request){
-  await connect()
+export async function POST(request) {
+  await connect();
 
-  const {url} = await request.json()
-  if(url===''){
+  const { url } = await request.json();
+  if (url === '') {
     return NextResponse.json({ message: 'Url is required!', success: false }, { status: 403 });
   }
 
-  const videoCode = GetYoutubeCode(url)
+  const videoCode = GetYoutubeCode(url);
 
-  if(!videoCode){
-      return NextResponse.json({ message: 'Invalid youtube url!', success: false }, { status: 403 });
+  if (!videoCode) {
+    return NextResponse.json({ message: 'Invalid youtube url!', success: false }, { status: 403 });
   }
 
-  const embedUrl = 'https://www.youtube.com/embed/'+videoCode;
-  const thumbnail = 'http://i3.ytimg.com/vi/'+videoCode+'/hqdefault.jpg'
+  const embedUrl = 'https://www.youtube.com/embed/' + videoCode;
+  const thumbnail = 'http://i3.ytimg.com/vi/' + videoCode + '/hqdefault.jpg';
 
-  try{
+  try {
     // Create media in db
     const isCreated = await Media.create({
-        name: 'youtube',
-        url: embedUrl,
-        alt: 'youtube',
-        type:'embed',
-        thumbnail:thumbnail
-      });
-  
-      if (isCreated) {
-        return NextResponse.json({ message: 'Url saved successfully!', success: true }, { status: 200 });
-      }
-      return NextResponse.json({ message: 'Url not saved!', success: false }, { status: 500 });
-  }catch(error){
-    return  NextResponse.json({message:'Internal Server Error!',success: false})   
+      name: 'youtube',
+      url: embedUrl,
+      alt: 'youtube',
+      type: 'embed',
+      thumbnail: thumbnail,
+    });
+
+    if (isCreated) {
+      return NextResponse.json({ message: 'Url saved successfully!', success: true }, { status: 200 });
+    }
+    return NextResponse.json({ message: 'Url not saved!', success: false }, { status: 500 });
+  } catch (error) {
+    return NextResponse.json({ message: 'Internal Server Error!', success: false });
   }
 }
--- src/app/api/media/route.js
+++ src/app/api/media/route.js
@@ -1,49 +1,48 @@
-import Media from '@/models/media'
+import Media from '@/models/media';
 import { NextResponse } from 'next/server';
-import {connect} from '@/DB/index'
-import * as Yup from "yup";
+import connect from '@/lib/db';
+import * as Yup from 'yup';
 
-export async function GET(){
-  await connect()
-  try{
+export async function GET() {
+  await connect();
+  try {
     const allMedia = await Media.find({});
-    return  NextResponse.json({media:allMedia,success: true})   
-  }catch(error){
-    return  NextResponse.json({message:'Internal Server Error!',success: false})   
+    return NextResponse.json({ media: allMedia, success: true });
+  } catch (error) {
+    return NextResponse.json({ message: 'Internal Server Error!', success: false });
   }
 }
 
-export async function PUT(request){
-  await connect()
+export async function PUT(request) {
+  await connect();
 
-  const data = await request.json()
-  (data)
+  const data = await request.json()(data);
   const MediaVal = Yup.object({
     id: Yup.string().required('Id is required!'),
     name: Yup.string().required('Name is required!'),
     alt: Yup.string().required('Alt is required!'),
-  })
-  
-  try{
-    await MediaVal.validate(data, {abortEarly: false});
-  }catch(error){
-    return  NextResponse.json({message:'Internal Server Error!',success: false})   
+  });
+
+  try {
+    await MediaVal.validate(data, { abortEarly: false });
+  } catch (error) {
+    return NextResponse.json({ message: 'Internal Server Error!', success: false });
   }
 
+  const { id, name, alt } = data;
 
-  const {id,name,alt} = data;
-  
-  try{
-    const isUpdated = await Media.findByIdAndUpdate(id,{
-      name,alt
+  try {
+    const isUpdated = await Media.findByIdAndUpdate(id, {
+      name,
+      alt,
     });
 
-    if(!isUpdated){
-      return  NextResponse.json({message:'Something went wrong!',success: false})   
+    if (!isUpdated) {
+      return NextResponse.json({ message: 'Something went wrong!', success: false });
     }
 
-    return  NextResponse.json({message:'Media Updated!',success: true})   
-  }catch(error){
-    return  NextResponse.json({message:'Internal Server Error!',success: false})   
+    return NextResponse.json({ message: 'Media Updated!', success: true });
+  } catch (error) {
+    return NextResponse.json({ message: 'Internal Server Error!', success: false });
   }
 }
