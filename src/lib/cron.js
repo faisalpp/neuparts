b06e2d9df0503e302c4bdac3fca627d37f9f0b2f
faisalpp <muhammadfaisal522@gmail.com>
2024-11-01 23:22:00 +0500

    neulink v1.1.0

--- src/lib/cron.js
+++ src/lib/cron.js
@@ -1,10 +1,8 @@
 
 
 async function test(){ 
-  const res = await fetch('http://localhost:3000/api/admin/neulink-products')
+  const res = await fetch(`${process.env.NEXT_BASE_API}/api/admin/neulink-products`)
   const data = await res.json()
-  
-  console.log(data)
 }
 
 test()
